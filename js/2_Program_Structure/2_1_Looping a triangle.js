881acdca409446f09264eac55f3feec734519c03
YakovcevMark <YakovcevMark@gmail.com>
2023-02-10 15:55:50 +0300

    work on mistakes

---
+++ js/2_Program_Structure/2_1_Looping a triangle.js
@@ -0,0 +1,17 @@
+/*
+Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+"use strict";
+for (let i = 1; i <= 7; i++){
+    console.log("#".repeat(i));
+}
+
