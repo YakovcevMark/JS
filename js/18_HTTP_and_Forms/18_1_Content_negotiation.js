c4c5da8f30002f2c3fdb76db4f01ce519edafdeb
YakovcevMark <YakovcevMark@gmail.com>
2023-02-23 19:18:06 +0300

    added 'http and forms'

---
+++ js/18_HTTP_and_Forms/18_1_Content_negotiation.js
@@ -0,0 +1,24 @@
+/*
+The URL https://eloquentjavascript.net/author is configured to respond
+with either plaintext, HTML, or JSON, depending on what the client asks for.
+These formats are identified by the standardized media types text/plain,
+text/html, and application/json.
+
+Send requests to fetch all three formats of this resource.
+Use the headers property in the options object passed to fetch to set
+the header named Accept to the desired media type.
+
+Finally, try asking for the media type application/rainbows+unicorns
+and see which status code that produces.
+*/
+let url = "https://eloquentjavascript.net/author";
+let types = ["text/plain",
+             "text/html",
+             "application/json",
+             "application/rainbows+unicorns"];
+
+types.forEach(item =>
+    fetch(url, {headers: {accept: item}})
+    .then(resp => resp.text())
+    .then(text => console.log(`Type : ${item} \n ${text}`)));
+
